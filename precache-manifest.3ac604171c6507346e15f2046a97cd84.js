d65a05a3945bfd8066041b1e6ccdcb4a40cbea56
Yurii Lisovenko <yriyliso@gmail.com>
2021-06-07 15:12:43 +0300

    Updates

---
+++ precache-manifest.3ac604171c6507346e15f2046a97cd84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29de19cea7e3b7fdabb60c2717fde693",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca306ddd3f76be2289c3",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "e13b7944adfc7dc33960",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "ca306ddd3f76be2289c3",
+    "url": "./static/js/2.db3a82cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.db3a82cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13b7944adfc7dc33960",
+    "url": "./static/js/main.a2eba6e5.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
